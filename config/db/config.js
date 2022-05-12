ab12a31c1390c5f41c1b5f2e252c5a715d63bd9c
Roy <roymayorga10@gmail.com>
2022-05-12 19:03:19 +0800

    security update

--- config/db/config.js
+++ config/db/config.js
@@ -12,15 +12,3 @@ module.exports = {
   	database: 'issuelogger'
 
 }
-
-
-
-
-
-
-// # Access the database with this cmd setup 
-// # mysql --host=us-cdbr-east-05.cleardb.net --user=bc2a0cb1f8df38 --password=9af74747 --reconnect heroku_542edf34eb684c9
-
-/*
-	recieve an error from heroku if set the db as env
-*/
